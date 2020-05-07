1de3df5210f2af94d6cb78d1b906b22f5ded6eb4
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2020-05-07 18:38:56 -0300

    Adding more comments

--- airflow_home/dags/songs_etl.py
+++ airflow_home/dags/songs_etl.py
@@ -1,13 +1,15 @@
+# NOTE: Importing from aws_utils - custom code folder inside plugins
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from aws_utils.emr_deployment import create_iam_role, create_s3_buckets, upload_code, create_emr_cluster
 
-
+# Creates the songs_etl dag, to be executed daily.
 dag = DAG(dag_id='songs_etl', description='Simple tutorial DAG',
           schedule_interval='@daily',
           start_date=datetime(2017, 3, 20), catchup=False)
 
+# Usage of PythonOperators with the imported custom code from plugins
 create_emr_role = PythonOperator(task_id='create_emr_role',
                                  python_callable=create_iam_role,
                                  dag=dag
@@ -27,6 +29,7 @@ run_on_emr = PythonOperator(task_id='run_on_emr',
                             python_callable=create_emr_cluster,
                             dag=dag)
 
+# Definition of the DAG execution order
 create_emr_role
 create_buckets
 create_buckets >> upload_code
--- airflow_home/dags/terminate_emr_cluster.py
+++ airflow_home/dags/terminate_emr_cluster.py
@@ -3,10 +3,12 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from aws_utils.emr_deployment import terminate_stale_clusters
 
+# Creating a simple dag, to be executed hourly
 dag = DAG(dag_id='terminate_emr_clusters', description='Simple tutorial DAG',
           schedule_interval='@hourly',
           start_date=datetime(2017, 3, 20), catchup=False)
 
+# Declaring a single operator in this dag, so it's picked up by Airflow
 terminate_clusters = PythonOperator(task_id='terminate_emr_clusters',
                                     python_callable=terminate_stale_clusters,
                                     dag=dag
